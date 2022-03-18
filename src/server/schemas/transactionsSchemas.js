0520ad5584d32ca6138ef769c7e031a73bea045b
Mario González <mario@mariogl.com>
2022-03-18 16:05:54 +0100

    pf

--- src/server/schemas/transactionsSchemas.js
+++ src/server/schemas/transactionsSchemas.js
@@ -1,9 +1,12 @@
 const { Joi } = require("express-validation");
+Joi.objectId = require("joi-objectid")(Joi);
 
 const transactionSchema = {
   body: Joi.object({
+    id: Joi.objectId().allow(""),
     description: Joi.string().required(),
     quantity: Joi.number().required(),
+    category: Joi.objectId().required(),
     date: Joi.date().required(),
   }),
 };
